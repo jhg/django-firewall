fbd839625827cc23159112443f3704fa1db3b814
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2014-04-04 00:30:09 +0100

    Delete Javascript that not is necesary

--- django_firewall/admin.py
+++ django_firewall/admin.py
@@ -1,27 +1,17 @@
 from django.contrib import admin
 from models import Rule, Log
 from forms import RuleForm
-from django.conf import settings
 
 class RuleAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active', 'action', 'source', 'source_range_end', 'position')
     list_filter = ('is_active', 'action',)
     list_editable = ('is_active', 'position', )
+    list_per_page = 50
     
     form = RuleForm
     
     class Meta:
         models = Rule
-        
-    class Media:
-        js = (
-            settings.STATIC_URL + 'firewall/js/jquery-1.7.1.min.js',
-            settings.STATIC_URL + 'firewall/js/jquery-ui-1.8.17.custom.min.js',
-            settings.STATIC_URL + 'firewall/js/menu-sort.js',
-        )
-
-class RuleInline(admin.StackedInline):
-    model = Rule
 
 class LogAdmin(admin.ModelAdmin):
     list_display = ('datetime', 'passed_firewall', 'method', 'response_status_code', 'ip',  'path', 'client',)
